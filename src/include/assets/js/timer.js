729c439425c2d6bee79300d2ec24e3b12249bc80
Jakub <jakubgranos@gmail.com>
2019-02-17 22:17:38 +0100

    dodanie sekcji header

---
+++ src/include/assets/js/timer.js
@@ -0,0 +1,12 @@
+function timerUpdate() {        
+	var currentTime = new Date();
+	var h = currentTime.getHours();
+	var m = currentTime.getMinutes();
+	var s = currentTime.getSeconds();
+
+	setTimeout(timerUpdate,1000);
+	document.getElementById("hours").innerHTML = h + '<span>hours</span>';
+	document.getElementById("minutes").innerHTML = m + '<span>minutes</span>';
+	document.getElementById("seconds").innerHTML = s + '<span>seconds</span>';
+}
+timerUpdate();
