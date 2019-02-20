36aff3fe2774f350b6c3330326380e1c5a1ca9c8
Jakub <jakubgranos@gmail.com>
2019-02-20 21:27:52 +0100

    stworzenie hamburger menu + js, rwd

--- src/include/assets/js/timer.js
+++ src/include/assets/js/timer.js
@@ -5,8 +5,9 @@ function timerUpdate() {
 	var s = currentTime.getSeconds();
 
 	setTimeout(timerUpdate,1000);
+	
 	document.getElementById("hours").innerHTML = h + '<span>hours</span>';
 	document.getElementById("minutes").innerHTML = m + '<span>minutes</span>';
-	document.getElementById("seconds").innerHTML = s + '<span>seconds</span>';
+	document.getElementById("seconds").innerHTML = s + '<span>seconds </span>';
 }
 timerUpdate();
