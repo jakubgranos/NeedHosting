18688456ced410b1d168f9571596a6ffe2a01905
Jakub <jakubgranos@gmail.com>
2019-02-22 03:08:11 +0100

    poprawa skoku zegara

--- src/include/assets/js/timer.js
+++ src/include/assets/js/timer.js
@@ -1,22 +1,22 @@
 const hour = document.getElementById("hours");
 const min = document.getElementById("minutes");
 const sec = document.getElementById("seconds");
-let hourCount = 56;
-let minCount = 59;
-let secCount = 59;
+let hourCount = 23;
+let minCount = 58;
+let secCount = 58;
 function timer() {
   sec.innerHTML = secCount;
   --secCount;
   if (secCount <= -1) {
-    secCount = 59;
+    secCount = 58;
     min.innerHTML = minCount;
     --minCount;
     if (minCount <= -1) {
-      minCount = 59;
+      minCount = 58;
       hour.innerHTML = hourCount;
       --hourCount;
       if (hourCount <= -1) {
-        hourCount = 56;
+        hourCount = 23;
       }
     }
   }
