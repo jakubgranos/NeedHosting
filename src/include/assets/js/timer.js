9b4a91801dac9493a7e7eff24f1dc797173d86cf
Jakub <jakubgranos@gmail.com>
2019-02-22 02:45:44 +0100

    zmiana skryptu zegara na poprawny

--- src/include/assets/js/timer.js
+++ src/include/assets/js/timer.js
@@ -1,13 +1,24 @@
-function timerUpdate() {        
-	var currentTime = new Date();
-	var h = currentTime.getHours();
-	var m = currentTime.getMinutes();
-	var s = currentTime.getSeconds();
-
-	setTimeout(timerUpdate,1000);
-	
-	document.getElementById("hours").innerHTML = h + '<span>hours</span>';
-	document.getElementById("minutes").innerHTML = m + '<span>minutes</span>';
-	document.getElementById("seconds").innerHTML = s + '<span>seconds </span>';
+const hour = document.getElementById("hours");
+const min = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
+let hourCount = 56;
+let minCount = 59;
+let secCount = 59;
+function timer() {
+  sec.innerHTML = secCount;
+  --secCount;
+  if (secCount <= -1) {
+    secCount = 59;
+    min.innerHTML = minCount;
+    --minCount;
+    if (minCount <= -1) {
+      minCount = 59;
+      hour.innerHTML = hourCount;
+      --hourCount;
+      if (hourCount <= -1) {
+        hourCount = 56;
+      }
+    }
+  }
 }
-timerUpdate();
+const setTime = setInterval(timer, 1000);
