1a9e8d8b0f6f46c80e67451ae24b8d89723e30ee
Jakub <jakubgranos@gmail.com>
2019-03-07 01:56:41 +0100

    poprawa kodu timer.js - po odswiezeniu nie resetuje się

--- docs/assets/js/timer.js
+++ docs/assets/js/timer.js
@@ -1,24 +1,46 @@
 const hour = document.getElementById("hours");
 const min = document.getElementById("minutes");
 const sec = document.getElementById("seconds");
-let hourCount = 23;
-let minCount = 58;
-let secCount = 58;
+
+const currentDate = new Date();
+let currentHour = currentDate.getHours();
+let currentMin= currentDate.getMinutes();
+let currentSec = currentDate.getSeconds();
+
+const counterDate = new Date("October 29, 1999 23:59:59");
+let counterHour = counterDate.getHours();
+let counterMin = counterDate.getMinutes();
+let counterSec = counterDate.getSeconds();
+
+
+//obliczenia
+let  subtractionSec = counterSec - currentSec;
+let  subtractionMin = counterMin - currentMin;
+let  subtractionHour = counterHour - currentHour;
 function timer() {
-  sec.innerHTML = secCount;
-  --secCount;
-  if (secCount <= -1) {
-    secCount = 58;
-    min.innerHTML = minCount;
-    --minCount;
-    if (minCount <= -1) {
-      minCount = 58;
-      hour.innerHTML = hourCount;
-      --hourCount;
-      if (hourCount <= -1) {
-        hourCount = 23;
-      }
-    }
-  }
+	sec.innerHTML = subtractionSec;
+	 --subtractionSec;
+	if(subtractionSec === 0){
+		subtractionSec = 59;
+		--subtractionSec;
+		
+		min.innerHTML = subtractionMin;
+		--subtractionMin;
+		if(subtractionMin === 0) {
+			subtractionMin = 59;
+			--subtractionMin;
+
+			hour.innerHTML = subtractionHour;
+			--subtractionHour;
+			if(subtractionHour === 0) {
+				subtractionhour = 23;
+				--subtractionhour;
+			}
+		}
+	}
+	sec.innerHTML = subtractionSec;
+	min.innerHTML = subtractionMin;
+	hour.innerHTML = subtractionHour;
 }
 const setTime = setInterval(timer, 1000);
+
