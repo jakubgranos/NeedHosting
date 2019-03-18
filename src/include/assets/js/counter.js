5dd3dd2b9f1c97df32e46ef28dde779c7c133154
Jakub <jakubgranos@gmail.com>
2019-03-18 23:51:04 +0100

    ulepszenie kodu counter.js - dodanie scroll offset, poprawa stylizacji strony, dodanie nowej sekcji

--- src/include/assets/js/counter.js
+++ src/include/assets/js/counter.js
@@ -2,15 +2,20 @@ const counterElement = document.getElementById('counter-user');
 const counterElement2 = document.getElementById('counter-newspaper');
 const counterElement3 = document.getElementById('counter-comments');
 const counterElement4 = document.getElementById('counter-envelope');
+
+const counterWrapper = document.getElementById('counter');
 let startTime = 0;
 function myTimer() {
-	counterElement.innerHTML = startTime;
-	counterElement2.innerHTML = startTime;
-	counterElement3.innerHTML = startTime;
-	counterElement4.innerHTML = startTime;
-	startTime++;
-	if(startTime >= 1000){
-		clearInterval(stopTimer);
+	if(window.pageYOffset > 1100){
+		counterWrapper.className = "counter fadein";
+		counterElement.innerHTML = startTime;
+		counterElement2.innerHTML = startTime;
+		counterElement3.innerHTML = startTime;
+		counterElement4.innerHTML = startTime;
+		startTime++;
+		if(startTime >= 1000){
+			clearInterval(stopTimer);
+		}
 	}
 
 }
