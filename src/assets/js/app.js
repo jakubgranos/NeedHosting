cbdd66f960e40e1a0bf70f35d3c6ef9bc82fa3eb
Jakub <jakubgranos@gmail.com>
2019-03-27 22:43:00 +0100

    dodanie nowej sekcji client-slider, dodanie do tej sekcji carousel, dodanie sliper do client-slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,10 @@
 import getWindowUserAgent from './modules/userAgent';
-
 window.onload = () => {
 	console.log('You are using:', getWindowUserAgent());
 }
+
+import "./modules/timer";
+import "./modules/counter";
+import "./modules/hamburger-menu";
+import "./modules/sticky";
+import "./modules/swiper";
---
+++ src/assets/js/modules/swiper.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper';
+const slider = new Swiper('.swiper-container', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
